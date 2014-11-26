0093f58bb507b92a75f23b23037c02f93353b7fa
LudvigWalimaa <ludwa930@student.liu.se>
2014-11-27 00:33:18 +1100

    Initial preparations

---
+++ app/views/layout/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Parallax | Ludvig W&#228;limaa</title>
+		<meta http-equiv="Content-Type" content="text/html" charset="utf8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+		
+
+		<link href="css/xsmall.css" rel="stylesheet">
+		<link href="css/small.css" rel="stylesheet" media="screen and (min-width: 768px)">
+		<link href="css/medium.css" rel="stylesheet" media="screen and (min-width: 992px)">
+		<link href="css/large.css" rel="stylesheet" media="screen and (min-width: 1200px)">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		<script src="http://code.jquery.com/jquery.js"></script>
+	</head>
+<body>
+
+<h1>hej</h1>
+
+
+</body>
+</html>
