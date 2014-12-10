45f2f7fd92fe694626b0848f1f8957f330c74142
LudvigWalimaa <ludwa930@student.liu.se>
2014-12-10 22:02:16 +1100

    Små ändringar

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -7,7 +7,7 @@
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 		<link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 		
-
+		<link href="css/index.css" rel="stylesheet">
 		<link href="css/xsmall.css" rel="stylesheet">
 		<link href="css/small.css" rel="stylesheet" media="screen and (min-width: 768px)">
 		<link href="css/medium.css" rel="stylesheet" media="screen and (min-width: 992px)">
@@ -25,7 +25,14 @@
 <body>
 
 <h1>hej</h1>
-
+<div id="face">
+  <div id="content">
+    <p><input type="text" placeholder="Name" /></p>
+    <p><input type="text" placeholder="Email" /></p>
+    <p><textarea placeholder="Comments"></textarea></p>
+    <p><input type="button" value="SUBMIT" /></p>
+  </div>
+</div>
 
 </body>
 </html>
